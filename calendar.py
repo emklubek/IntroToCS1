71d513206bd7fa07a4c800e6102d0cd68520f812
Erin Klubek <42846547+emklubek@users.noreply.github.com>
2018-09-26 09:37:43 -0500

    Update calendar.py

--- calendar.py
+++ calendar.py
@@ -8,5 +8,19 @@ print("Welcome to the Calendar program.")
 
 
 #Determine Days (week and month)
+century_digits = format(year/100, '.0f')
+year_digits = year%100
+value = year_digits+floor(year_digits/4)
+if century_digits = 18:
+  value = value + 2
+elif century_digits = 20:
+  value = value + 6
+if month = 'January' and not (year%4==0):
+  value = value + 1
+elif month = 'February' and (year%4==0):
+  value = value + 3
+  if month = 'February' and not (year%4==0):
+    value = value + 4
+elif
 
-#Print and Format Results
+#Print and Format results
