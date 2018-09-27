5f7ddc4d6766aa54bd2f97b3b10fa50bae6f96a9
Erin Klubek <42846547+emklubek@users.noreply.github.com>
2018-09-27 17:19:49 -0500

    Update calendar.py

--- calendar.py
+++ calendar.py
@@ -11,6 +11,7 @@ print("Welcome to the Calendar program.")
 century_digits = format(year/100, '.0f')
 year_digits = year%100
 value = year_digits+floor(year_digits/4)
+
 if century_digits = 18:
   value = value + 2
 elif century_digits = 20:
@@ -21,6 +22,37 @@ elif month = 'February' and (year%4==0):
   value = value + 3
   if month = 'February' and not (year%4==0):
     value = value + 4
-elif
+elif month = 'March' or month = 'November':
+  value = value + 4
+elif month = 'April' or month = 'July':
+  value = value + 0
+elif month = 'May':
+  value = value + 2
+elif month = 'June':
+  value = value + 5
+elif month = 'August':
+  value = value + 3
+elif month = 'October':
+  value = value + 1
+elif month = 'September' or month = 'December':
+  value = value + 6
+  
+value = (value + day)mod 7
+
+if value == 1:
+  weekday = Sunday
+if value == 2:
+  weekday = Monday
+if value == 3:
+  weekday = Tuesday
+if value == 4:
+  weekday = Wednesday
+if value == 5:
+  weekday = Thursday
+if value == 6:
+  weekday = Friday
+if value == 0:
+  weekday = Saturday
+
 
 #Print and Format results
