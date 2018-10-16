b1ffff3148a1fc847ab70ff1a8de6036350b89b2
Erin Klubek <42846547+emklubek@users.noreply.github.com>
2018-10-16 08:06:57 -0500

    Update treasureHunt.py

--- treasureHunt.py
+++ treasureHunt.py
@@ -1,6 +1,18 @@
 #Treasure Hunt Game 
 #10-11-2018
 
+def easy():
+  turn = 5
+  while turn >= 0:
+    
+def medium():
+  turn = 7
+  while turn >= 0:
+  
+def hard(): 
+  turn = 9
+  while turn >= 0:
+
 #Greeting with a little bit of piracy
 start = input("Welcome to the Treasure Hunt Game. \nAre you ready to hunt for some treasure?")
 if start in (Yes, yes, yeah, Yeah, ya, Ya):
@@ -11,6 +23,3 @@ else:
 #Random things to come later on in the program
 difficulty = input("What difficulty do you want to play on? (easy, medium, or hard)")
 
-#for easy
-turn = 3
-while turn > 0:
