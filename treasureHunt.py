6f479f77119b1456c9ac45a8a9a6585fc1627682
Erin Klubek <42846547+emklubek@users.noreply.github.com>
2018-10-18 14:16:36 -0500

    Update treasureHunt.py

--- treasureHunt.py
+++ treasureHunt.py
@@ -2,6 +2,7 @@
 #10-11-2018
 
 import random
+import pprint
 
 #main part of the program with difficulty selection and function calling
 def main():
@@ -17,23 +18,69 @@ def main():
 
 def easy():
   turn = 7
+  counter = 0
   grid = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
   treasures = [' ', ' ', ' ', ' ', ' ']
-  for i in range(1,5):
-    random_num = random.randint(1,9)
-  treasures.i
+  treasures[0] = random.randint(1,9)
+  treasures[1] = random.randint(1,9)
+  treasures[2] = random.randint(1,9)
+  treasures[3] = random.randint(1,9)
+  treasures[4] = random.randint(1,9)
+  
   
   while turn >= 0:
-    print(grid[0])
-    print(grid[1])
-    print(grid[2])
+    turn = turn - 1
+    pprint.pprint(grid)
     guess = input("Where do ye want t' dig fer treasure? (Enter a number between 1 and 9)")
     if guess in treasures:
-      print("Thank ye for finding a treasure! Can ye find th' rest?")
-    elif:
+      counter = counter + 1
+      if guess == 1:
+        grid[0][0] = 'X'
+      elif guess == 2:
+        grid[0][1] = 'X'
+      elif guess == 3:
+        grid[0][2] = 'X'
+      elif guess == 4:
+        grid[1][0] = 'X'
+      elif guess == 5:
+        grid[1][1] = 'X'
+      elif guess == 6:
+        grid[1][2] = 'X'
+      elif guess == 7:
+        grid[2][0] = 'X'
+      elif guess == 8:
+        grid[2][1] = 'X'
+      elif guess == 9:
+        grid[2][2] = 'X'
+     
+      if counter < 5: 
+        print("Thank ye for finding a treasure! Can ye find th' rest?")
+      elif counter == 5:
+        print("Thank ye fer finding all me treasure!")
+    
+    elif guess not in treasures:
+      if guess == 1:
+        grid[0][0] = 'O'
+      if guess == 2:
+        grid[0][1] = 'O'
+      if guess == 3:
+        grid[0][2] = 'O'
+      if guess == 4:
+        grid[1][0] = 'O'
+      if guess == 5:
+        grid[1][1] = 'O'
+      if guess == 6:
+        grid[1][2] = 'O'
+      if guess == 7:
+        grid[2][0] = 'O'
+      if guess == 8:
+        grid[2][1] = 'O'
+      if guess == 9:
+        grid[2][2] = 'O'
       print("Me treasure ain't there matey! Try Again.")
     print("You have", turn, "turns left")
-    turn = turn - 1
+  if counter < 5:
+    print("Ye didn' find enough treasure.... Now ye must walk the plank. Argh!")
     
 
     
